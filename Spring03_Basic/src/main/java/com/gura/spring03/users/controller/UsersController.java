18c038e1229c37255b0122964568989a61ceacc6
leechangho <maltitol7090@gmail.com>
2020-01-29 16:10:03 +0900

    이것저것 추가

--- Spring03_Basic/src/main/java/com/gura/spring03/users/controller/UsersController.java
+++ Spring03_Basic/src/main/java/com/gura/spring03/users/controller/UsersController.java
@@ -4,8 +4,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.gura.spring3.users.dto.UsersDto;
 
 @Controller
 public class UsersController {
@@ -36,6 +40,61 @@ public class UsersController {
 		request.setAttribute("isSuccess", isSuccess);
 		//view page의 정보를 리턴한다
 		return "users/login";
+	}
+	
+	@RequestMapping("/users/login2")
+	public ModelAndView login2(@RequestParam String id, 
+			@RequestParam String pwd, HttpSession session,
+			ModelAndView mView) {
+		//유효한 정보인지 여부
+		boolean isSuccess=false;
+		if(id.equals("gura") && pwd.equals("1234")){
+			isSuccess=true;
+			//로그인처리를 한다
+			session.setAttribute("id", id);
+		}
+		//view 페이지에서 필요한 모델을 담고
+		mView.addObject("isSuccess", isSuccess);
+		//view page의 정보도 담고
+		mView.setViewName("users/login");
+		//리턴해준다
+		return mView;
+	}
+	/*
+	 *  @ModelAttribute 어노테이션과 함께 Dto를 메소드의 인자로 선언하면
+	 *  전송되는 파라미터가 자동 추출되어서 Dto객체에 담겨서 인자로 전달된다.
+	 *  단, 전송되는 파라미터명과 Dto의 필드명이 같아야한다.
+	 * 
+	 */
+	
+	@RequestMapping("/users/login3")
+	public ModelAndView login3(@ModelAttribute UsersDto dto,
+			HttpSession session, ModelAndView mView) {
+		//유효한 정보인지 여부
+		boolean isSuccess=false;
+		if(dto.getId().equals("gura") && dto.getPwd().equals("1234")){
+			isSuccess=true;
+			//로그인처리를 한다
+			session.setAttribute("id", dto.getId());
+		}
+		//view 페이지에서 필요한 모델을 담고
+		mView.addObject("isSuccess", isSuccess);
+		//view page의 정보도 담고
+		mView.setViewName("users/login");
+		//리턴해준다
+		return mView;
+	}
+	
+	//로그아웃 요청 처리하는 메소드
+	@RequestMapping("/users/logout")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		/*	
+		 * 	forward 이동이 아닌 리다이렉트 이동 응답을 하려면 view page의 정보를
+		 * 	"redirect:리다이렉트 시킬 절대경로" 형식으로 작성하면 된다.
+		 * 	단, context path는 자동으로 붙으니 작성하지 않는다.
+		 */
+		return "redirect:/home.do";
 	}
 }
 
